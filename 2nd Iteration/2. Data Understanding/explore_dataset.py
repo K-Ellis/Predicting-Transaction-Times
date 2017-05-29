437bb15e1bdf84117b4f02a97858adbc7d920fe2
Kieron Ellis <kieron.ellis@ucdconnect.ie>
2017-05-29 14:00:31 +0100

    create explore ipynb

--- 2nd Iteration/2. Data Understanding/explore_dataset.py
+++ 2nd Iteration/2. Data Understanding/explore_dataset.py
@@ -4,10 +4,14 @@ df = pd.read_csv("../../../Data/vw_Incident.csv", encoding='latin-1')
 #
 # print(df.head())
 #
-# print(df.describe())
+print(df.iloc[:,0].describe())
 
 # print(df.iloc[4589,16])
 # print(df[df["SubSubReason"] == "RR Contig"])
+
+## This only appears once in the dataset, therefore it should probably be
+# removed for modelling.
 print(df["SubSubReason"].loc[df["SubSubReason"] == "RR Contig"])
 
+## This only appears twice, remove it.
 print(df["Referencesystem"].loc[df["Referencesystem"] == "eMSL"])
