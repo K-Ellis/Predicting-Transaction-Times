a7fbccc55aa04019f7583e7ccfbb2d8c3f67cea3
Eoin Carroll <eoin.carroll1@ucdconnect.ie>
2017-06-06 17:42:27 +0100

    Commented out one hot countries
    One hot encoded countries for testing. Commented out the code for now

--- 2nd Iteration/3. Data Preparation/prepare_dataset.py
+++ 2nd Iteration/3. Data Preparation/prepare_dataset.py
@@ -222,12 +222,15 @@ def clean_Incident():
     # todo replace the Null values with the mean for the column
     # df["CaseRevenue"] = df["CaseRevenue"].fillna(df["CaseRevenue"].mean())
 
+    """
+    # Used for testing model program - can delete whenever
     one_hot_encoding(df, "CountrySource", out_file)
     one_hot_encoding(df, "CountryProcessed", out_file)
     one_hot_encoding(df, "SalesLocation", out_file)
     del df["CountrySource"]
     del df["CountryProcessed"]
     del df["SalesLocation"]
+    """
 
 
     df.to_csv("../../../Data/vw_Incident_cleaned.csv", index = False)   # export file
