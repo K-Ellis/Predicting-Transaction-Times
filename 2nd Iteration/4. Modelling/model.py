e9670b66ee5e700450de1f75f39e3aa60660ad69
Eoin Carroll <eoin.carroll1@ucdconnect.ie>
2017-06-07 18:54:37 +0100

    Cleaned model code

--- 2nd Iteration/4. Modelling/model.py
+++ 2nd Iteration/4. Modelling/model.py
@@ -18,15 +18,17 @@ Import libraries
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from pylab import polyfit
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
 from math import sqrt
+from pylab import polyfit
 
 
 def split_data(df):
+    trainData, testData = train_test_split(df, test_size=0.2)  # Split data 80:20 randomly
     trainData_y = pd.DataFrame()
     trainData_y["TimeTaken"] = trainData["TimeTaken"]
     trainData_x = trainData.loc[:, trainData.columns != 'TimeTaken']
@@ -54,12 +56,24 @@ def linear_regression(trainData_x, trainData_y, testData_x, testData_y):
     plt.show()
 
     print("rmse:", sqrt(mean_squared_error(testData_y, y_pred)))  # Print Root Mean Squared Error
+    # More tools in sklearn metrics
 
 
 if __name__ == "__main__":  # Run program
-    np.random.seed(12345)
+    np.random.seed(12345)  # Set seed
     df = pd.read_csv("../../../Data/preprocessed_data.csv", encoding='latin-1', low_memory=False)  # Read in csv file
-    trainData, testData = train_test_split(df, test_size=0.2)  # Split data 80:20 randomly
 
-    trainData_X, trainData_y, testData_X, testData_y = split_data(df)
-    linear_regression(trainData_X, trainData_y, testData_X, testData_y)
+    trainData_x, trainData_y, testData_x, testData_y = split_data(df)  # Split data
+
+
+    # if data is not well spaced out
+    # compare mean
+
+    # compare standard deviation
+    # print(mean(trainData_y))
+    # print(mean(testData_y))
+        # print("Data not well stratified, retrying . . .")
+        # trainData, testData = train_test_split(df, test_size=0.2)  # Split data 80:20 randomly
+
+
+    linear_regression(trainData_x, trainData_y, testData_x, testData_y)  # Linear Regression
