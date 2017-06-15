1286614dd0ce9ae9802892ab455233e8437b1fb6
K-Ellis <kieron.ellis@ucdconnect.ie>
2017-06-15 16:58:42 +0100

    print RMSE and R Squared on new line for each alg

--- 2nd Iteration/4. Modelling/model.py
+++ 2nd Iteration/4. Modelling/model.py
@@ -153,7 +153,7 @@ def results(testData_y, y_pred, trainData_y, y_train_pred, alg):
     print(alg, "Test rmse:", sqrt(mean_squared_error(testData_y, y_pred)))  # Print Root Mean Squared Error
 
     print(alg, "Train R^2 score:", r2_score(trainData_y, y_train_pred))  # Print R Squared
-    print(alg, "Test R^2 score:", r2_score(testData_y, y_pred))  # Print R Squared
+    print(alg, "Test R^2 score:", r2_score(testData_y, y_pred), "\n")  # Print R Squared
     # http://scikit-learn.org/stable/modules/generated/sklearn.metrics.r2_score.html
 
 
