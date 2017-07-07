2952fac480a3b5421f8a80946f5822fb8f63c65b
Naomi Raab <naomi.raab@ucdconnect.ie>
2017-07-07 15:48:49 +0200

    xgboost updates

--- 4th Iteration/4. Modelling/xgboost.py
+++ 4th Iteration/4. Modelling/xgboost.py
@@ -1,8 +1,9 @@
 ind = 1
 import xgboost
+import xgboost as xgb
 if ind == 1:
 	import pandas as pd
-	from xgboost import XGBRegressor
+	#from xgboost import XGBRegressor
 
 	df = pd.DataFrame({'X': [3, 4, 6], 'y': [12, 21, 87]})
 	X = df.drop('y', axis=1)
@@ -13,7 +14,7 @@ if ind == 1:
 		'n_estimators': 50,
 		'objective': 'reg:linear'}
 
-	model = XGBRegressor(**params)
+	model = xgboost.XGBRegressor(**params)
 	model.fit(X, y)
 
 	x_test = pd.DataFrame({'X': [4, 1, 7]})
