eab66a8deb0586b284fd23f37c85bb466a8dd969
K-Ellis <kieron.ellis@ucdconnect.ie>
2017-07-12 18:44:47 +0100

    create program to read list of parameters

--- 4th Iteration/3. Data Preparation/create_3_TimeTaken_classes.py
+++ 4th Iteration/3. Data Preparation/create_3_TimeTaken_classes.py
@@ -11,20 +11,49 @@ def split_df_into_3(bins, df):
 
 df = pd.read_csv("../../../Data/ABT_Incident_HoldDuration_cleaned.csv",encoding='latin-1',low_memory=False)
 
-bins = [92000, 370000]
-print(92000/60/60)
-print(370000/60/60/24)
-split_df_3 = split_df_into_3(bins, df)
+# bins = [92000, 370000]
+# print(92000/60/60)
+# print(370000/60/60/24)
+# split_df_3 = split_df_into_3(bins, df)
 
-print(len(split_df_3))
-lengthsdfs = [len(split_df_3[i]) for i in range(len(split_df_3))]
+# print(len(split_df_3))
+# lengthsdfs = [len(split_df_3[i]) for i in range(len(split_df_3))]
+# print(lengthsdfs)
+# print(sum(lengthsdfs))
+# print(len(df))
+
+# split_df_3[0].to_csv("../../../Data/split_small.csv", index=False)
+# split_df_3[1].to_csv("../../../Data/split_medium.csv", index=False)
+# split_df_3[2].to_csv("../../../Data/split_large.csv", index=False)
+
+def get_bins_daily(inbin, day, num):
+    bin_list = [inbin]
+    for i in range(num):
+        bin_list.append(bin_list[i]+day)
+    return bin_list
+
+def split_df_into_k_bins(df, num_bins):
+    bin1 = 8*60*60
+    day = 24*60*60
+    bins = get_bins_daily(bin1, day, num_bins)
+    dflist = []
+    prev_bin = 0
+    for bin0 in bins:
+        dflist.append(pd.DataFrame(df.loc[(df["TimeTaken"] < bin0) & (df["TimeTaken"] >= prev_bin)]))
+        prev_bin = bin0
+    dflist.append(pd.DataFrame(df.loc[df["TimeTaken"] >= bins[-1]]))
+    return dflist
+
+k_split_df = split_df_into_k_bins(df, num_bins=4)
+
+print(len(k_split_df))
+lengthsdfs = [len(k_split_df[i]) for i in range(len(k_split_df))]
 print(lengthsdfs)
 print(sum(lengthsdfs))
 print(len(df))
 
-split_df_3[0].to_csv("../../../Data/split_small.csv", index=False)
-split_df_3[1].to_csv("../../../Data/split_medium.csv", index=False)
-split_df_3[2].to_csv("../../../Data/split_large.csv", index=False)
+for i in range(len(k_split_df)):
+	k_split_df[i].to_csv("../../../Data/split_%s.csv" % i, index=False)
 
 # 1.
     # if TimeTaken is in range(x1, y1) assign the row the label "Short"
