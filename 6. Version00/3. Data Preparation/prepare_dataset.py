352292d1367e8498323edb60916c8f01dddf23f8
Eoin Carroll <eoin.carroll1@ucdconnect.ie>
2017-08-02 18:29:03 +0100

    Fixed keepers

--- 6. Version00/3. Data Preparation/prepare_dataset.py
+++ 6. Version00/3. Data Preparation/prepare_dataset.py
@@ -104,11 +104,16 @@ def deletions(df, d):  # Delete all columns apart from those listed
     if d["Rolling_Median"] == "y": keepers.append("Rolling_Median")
     if d["Rolling_Std"] == "y": keepers.append("Rolling_Std")
 
-    if d["ordinal_mapping"] == "y": keepers.append(["Priority", "Complexity", "StageName"])
+    if d["ordinal_mapping"] == "y":
+        cols = ["Priority", "Complexity", "StageName"]
+        for col in cols:
+            keepers.append(col)
 
     if d["onehot_encoding"] == "y":
-        keepers.append(["CountrySource", "CountryProcessed", "SalesLocation", "StatusReason", "SubReason", "ROCName",
-                        "sourcesystem", "Source", "Revenutype", "Queue"])
+        cols = ["CountrySource", "CountryProcessed", "SalesLocation", "StatusReason", "SubReason", "ROCName",
+                    "sourcesystem", "Source", "Revenutype", "Queue"]
+        for col in cols:
+            keepers.append(col)
 
     # New variables created using Created_On and ResolvedDate:
     # "Concurrent_open_cases", "Cases_created_within_past_8_hours", "Cases_resolved_within_past_8_hours", 
@@ -120,7 +125,8 @@ def deletions(df, d):  # Delete all columns apart from those listed
                     "AssignedToGroup_BPO", "AssignedToGroup_CRMT"]:
             keepers.append(col)
     if d["append_AuditDuration"] == "y": keepers.append("AuditDuration")
-    
+
+    print("Keepers:", keepers)
     for col in df.columns:
         if col not in keepers: del df[col]
     print("Deletions:", df.shape)
